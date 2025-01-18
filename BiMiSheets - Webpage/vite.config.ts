835d48875ddf1d517f8574555998a6f3e6009044
midfranc <marybeth.defrance@ugent.be>
2025-01-18 11:59:54 +0100

    Should hopefully work on the custom domain

--- BiMiSheets - Webpage/vite.config.ts
+++ BiMiSheets - Webpage/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/BiMiSheets_webpage/",
+  base: "/",
   plugins: [
     vue(),
     vueDevTools(),
